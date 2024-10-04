cd1e773d50ada7acdff377188804d795554c06d1
Na'imatul Lu'lu'a <naimatullulua@gmail.com>
2024-10-04 13:51:06 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,14 +4,15 @@ import seaborn as sns
 import streamlit as st
 
 # Load your datasets
-all_data = pd.read_csv("d:/KULIAH/SEMESTER VII/BANGKIT 2024/submission/dashboard/transaction_merged.csv")
+order_items= pd.read_csv("d:/KULIAH/SEMESTER VII/BANGKIT 2024/submission/data/order_items_dataset.csv")
+order_reviews= pd.read_csv("d:/KULIAH/SEMESTER VII/BANGKIT 2024/submission/data/order_reviews_dataset.csv")
 
 st.header("E-Commerce Dashboard")
 
 with st.sidebar:
     st.title("Na'imatul Lu'lu'a")
 
-page = st.sidebar.radio("Pilih Halaman:", ["Home", "Pertanyaan 1", "Pertanyaan 2", "Pertanyaan 3"])
+page = st.sidebar.radio("Pilih Halaman:", ["Home", "Pertanyaan 1", "Pertanyaan 2"])
 
 if page == "Home":
     st.write("Selamat datang di dashboard analisis data E-Commerce!")
@@ -22,7 +23,7 @@ elif page == "Pertanyaan 1":
     
     # Pertanyaan 1
     plt.figure(figsize=(10, 8))
-    sns.scatterplot(x='price', y='freight_value', data=all_data)
+    sns.scatterplot(x='price', y='freight_value', data=order_items)
     plt.title('Hubungan antara Freight Value dan Price')
     plt.xlabel('Price')
     plt.ylabel('Freight Value')
@@ -35,7 +36,7 @@ elif page == "Pertanyaan 2":
     st.subheader("Jumlah Transaksi Berdasarkan Rating")
     
     # Hitung jumlah transaksi per rating
-    transactions_per_rating = all_data.groupby('review_score')['order_id'].count()
+    transactions_per_rating = order_reviews.groupby('review_score')['order_id'].count()
 
     # Visualisasikan dengan bar chart
     plt.figure(figsize=(8, 6))
@@ -48,19 +49,4 @@ elif page == "Pertanyaan 2":
     st.pyplot(plt)
     plt.clf()  # Clear the figure
 
-elif page == "Pertanyaan 3":
-    st.subheader("Jumlah Pesanan Berdasarkan Kota")
-
-    order_per_kota = all_data.groupby('seller_city')['order_id'].count().sort_values(ascending=False).head(10)
-
-    plt.figure(figsize=(12, 8))
-    sns.barplot(x=order_per_kota.index, y=order_per_kota.values)
-    plt.title('Jumlah Order per Kota (Top 10)')
-    plt.xlabel('Kota')
-    plt.ylabel('Jumlah Order')
-    plt.xticks(rotation=90)
-    
-    # Show bar plot in Streamlit
-    st.pyplot(plt)
-    plt.clf()  # Clear the figure
 
